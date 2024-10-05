2af6eb549b521db952c371ab74703c991fd2eb61
Oswald-Che <cheoswald@yahoo.com>
2024-10-05 01:09:07 +0100

    fill all kitten controller actions and views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -8,18 +8,41 @@ class KittensController < ApplicationController
   end
 
   def new
-    @kiteen = Kitten.new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, :unprocessable_entity
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :edit, :unprocessable_entity
+    end
   end
 
   def delete
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:age, :name, :cuteness, :softness)
   end
 end
